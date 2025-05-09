aace38e51dacd459e81ff81be566c2f0dcf2fd5f
vynas <hamdabousnina@gmail.com>
2025-05-08 20:27:48 -0400

    add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About-section";
+import Footer from "./components/Footer";
 import HeroSection from "./components/Hero-section";
 import Navbar from "./components/Navbar";
 import ServicesSection from "./components/Services-section";
@@ -12,6 +13,7 @@ function App() {
       <About />
       <ServicesSection />
       <ToursSection />
+      <Footer />
     </>
   );
 }
