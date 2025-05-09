71ea39c2b5796eff29343a41e82d7142caa2f60d
vynas <hamdabousnina@gmail.com>
2025-05-08 20:09:44 -0400

    add services-section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About-section";
 import HeroSection from "./components/Hero-section";
 import Navbar from "./components/Navbar";
+import ServicesSection from "./components/Services-section";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <HeroSection />
       <About />
+      <ServicesSection />
     </>
   );
 }
